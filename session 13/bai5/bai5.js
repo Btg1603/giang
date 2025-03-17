91325b157f0bd293555c7c114e9fed6f1edc4aed
Btg1603 <gbuif12@gmail.com>
2025-03-17 10:46:56 +0700

    Add files via upload

---
+++ session 13/bai5/bai5.js
@@ -0,0 +1,5 @@
+var firstNumber=parseInt( prompt("Nhập số thứ nhất: ")) ;
+var secondNumber=parseInt( prompt("Nhập số thứ hai: ")) ;
+
+let sum = firstNumber + secondNumber;
+document.write("Tổng hai số là: "+sum+"");
