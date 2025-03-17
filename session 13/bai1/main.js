3c36dc775fde42fb926581d6d8eb7fb1d8b2d879
Btg1603 <gbuif12@gmail.com>
2025-03-17 10:34:54 +0700

    Add files via upload

---
+++ session 13/bai1/main.js
@@ -0,0 +1,6 @@
+// Hiển thị "Hello World!" trên console
+console.log("Hello World!")
+// Ghi "Hello World!" trực tiếp lên trang web
+document.write("Hello World!<br>");
+// Hiển thị "Hello World!" trong hộp thoại cảnh báo
+alert("Hello World!");
---
+++ session 13/bai3/bai3.js
@@ -0,0 +1,8 @@
+// Hiển thị câu chào trong tab console
+console.log("Welcome to Rikkei Academy");
+
+// Hiển thị câu trên màn hình trình duyệt
+document.write("Quyết tâm học lập trình JavaScript<br>");
+
+// Hiển thị hộp thoại thông báo
+alert("Chào mừng bạn đến RIKKEI");
